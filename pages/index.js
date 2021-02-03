a512481d3e99b45170ffddba52b80863699fbb96
Débora Roque Gross <debygross@gmail.com>
2021-02-03 17:37:51 +0000

    Quiz com perguntas

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
-import styled from 'styled-components'
-import db from '../db.json'
-import Widget from '../src/components/Widget/index'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
-import QuizBackground from '../src/components/QuizBackground'
-
+import React from 'react';
+import Image from 'next/image';
+// import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+import Widget from '../src/components/Widget/index';
+import QuizLogo from '../src/components/QuizLogo';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
 // function Title(props) { //propcidades/propriedades<3
 //   return <h1>{props.children}</h1>
@@ -17,38 +24,74 @@ import QuizBackground from '../src/components/QuizBackground'
 //     background-position: center;
 // `;
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
+/*export const RowContainer = styled.div`
+  text-align: center;
 `;
 
+export const Button = styled.button`
+  color: ${db.theme.colors.contrastText};
+  background: ${db.theme.colors.secondary};
+  font-size: 1em;
+  margin-top: 2em;
+  padding: 0.25em 6em;
+`;*/
+
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+  console.log('retorno do useState', name, setName);
+
   return (
-      <QuizBackground backgroundImage={db.bg}> 
-        <QuizContainer>
-          <Widget>
-            <Widget.Header>
-              <h1>Miraculous Ladybug</h1>
-              </Widget.Header>
-            <Widget.Content>
-              <p> Homenagem a minha pequena</p>
-            </Widget.Content>
-          </Widget>
-          <Widget>
-            <Widget.Content>
-              <h1>Quizes da galera</h1>
-              <p> Perguntas</p>
-            </Widget.Content>
-          </Widget>
-          <Footer/>
-        </QuizContainer>
-        <GitHubCorner projectUrl= "https://github.com/omariosouto"/>
-      </QuizBackground>
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz- Modelo Base</title>
+        <meta property="og:image" content={db.bg} />
+      </Head>
+      <QuizContainer>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <Widget.Img>
+              <h1>
+                <Image src="/images/logoladybug.png" alt="Logo Ladybug" width="300px" height="150px" />
+              </h1>
+            </Widget.Img>
+          </Widget.Header>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma submissão por meio do react');
+
+              // router manda para a proxima pagina
+            }}
+            >
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => {
+                  console.log(infosDoEvento.target.value);
+                  // State
+                  // name = infosDoEvento.target.value;
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz o seu nome para jogar!"
+                value={name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Vamos Jogar ${name}!`}
+              </Button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+            <p> Perguntas</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://github.com/omariosouto" />
+    </QuizBackground>
   );
 }
---
+++ src/components/Head/index.js
@@ -0,0 +1,15 @@
+/* import Head from 'next/head'
+
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title>My page title</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  )
+}
+
+export default IndexPage NAO É NECESSARIO CRIAR ESSE COMPONENTE POIS JA VEM NA PASTA NODE_modules */
